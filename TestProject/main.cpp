69a17e80e8b5becb5786cdfd802e06ff66e7f096
maxim <horbatyukitstep@gmail.com>
2022-02-01 13:57:35 +0200

    added point constructor

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -5,9 +5,19 @@ using namespace std;
 class point {
 	int x, y, z;
 
+public:
+	point() {
+		x = y = z = 0;
+	}
+
+	void print() {
+		cout << x << " | " << y<<" | " << z;
+	}
 };
 
 int main() {
 	cout << "Hello world!\n";
+	point p;
+	p.print();
 	return 0;
 }
