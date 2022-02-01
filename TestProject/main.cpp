e1a7ba1630656af29dbc55d75dcbbdc388650883
maxim <horbatyukitstep@gmail.com>
2022-02-01 14:14:05 +0200

    Added some code

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -10,6 +10,12 @@ public:
 		x = y = z = 0;
 	}
 
+	point(int x, int y, int z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
 	void print() {
 		cout << x << " | " << y<<" | " << z;
 	}
@@ -28,14 +34,17 @@ public:
 	}
 
 	void print() {
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < size; i++) {
 			arr[i].print();
+			cout << "\n";
+		}
 	}
 };
 
 int main() {
 	cout << "Hello world!\n";
-	
-	p.print();
+	point points[2]{ point(1,2,3),point(1,3,2) };
+	shape s(2, points);
+	s.print();
 	return 0;
 }
