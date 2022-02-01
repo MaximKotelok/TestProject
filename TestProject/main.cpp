c7601c5686244fc98ba8a0b429019b8186c38427
maxim <horbatyukitstep@gmail.com>
2022-02-01 14:09:12 +0200

    Added shape

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -15,9 +15,27 @@ public:
 	}
 };
 
+class shape {
+	point* arr;
+	int size;
+public:
+	shape(int size, point* arr) {
+		this->size = size;
+
+		this->arr = new point[size];
+		for (int i = 0; i < size; i++)
+			this->arr[i] = arr[i];
+	}
+
+	void print() {
+		for (int i = 0; i < size; i++)
+			arr[i].print();
+	}
+};
+
 int main() {
 	cout << "Hello world!\n";
-	point p;
+	
 	p.print();
 	return 0;
 }
